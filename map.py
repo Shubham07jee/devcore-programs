35a365afbb47aa4b4e758f63fe723825a202bc40
Shubham Chaudhari <sc2631697@gmail.com>
2026-07-30 23:21:04 +0530

    Fix angle calculation output format

--- map.py
+++ map.py
@@ -19,8 +19,19 @@ while True:
     if minimum != maximum:
 
         angle = ((n - minimum) * 180) / (maximum - minimum)
-
+        
         print("Angle =",angle, "°")
     else:
         print("Need another different value")
 
+# Output:
+
+# Enter a value (0 to 1): 0.5
+# Minimum = 0.5
+# Maximum = 0.5
+# Need another different value
+
+# Enter a value (0 to 1): 0.8
+# Minimum = 0.5
+# Maximum = 0.8
+# Angle = 180.0 °
